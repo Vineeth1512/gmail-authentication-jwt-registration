22b8fae2ecd3f676f938b4acb48d7149c1b32154
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-01-28 10:52:40 +0530

    mongodbconnection

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express =require("express");
-const mongoose =require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 require('dotenv').config();
-const cors =require("cors");
-const userRoutes =require("./routes/user.route");
+const cors = require("cors");
+const userRoutes = require("./routes/user.route");
 const app = express();
 const portNo = process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
-app.use('/user',userRoutes);
+app.use('/user', userRoutes);
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     return res.status(200).json({
-        message:"Welcome to User Registration"
+        message: "Welcome to User Registration"
     })
 })
-const MONGODB_URL =process.env.DATABASE_URL
-mongoose.connect(MONGODB_URL).then(() => {
-    console.log("MongoDB is connected");
-
-    app.listen(portNo, () => {
-        console.log(`Server is running on ${portNo}`)
-    })
+app.listen(portNo, () => {
+    console.log(`Server is running on ${portNo}`)
 })
 
-module.exports =app;//for vercel
+const MONGODB_URL = process.env.DATABASE_URL
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB is connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
+module.exports = app;//for vercel
