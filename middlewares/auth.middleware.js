21ff06567ad4a589021c04615f18f3ebdcd0f5d4
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-01-30 14:07:50 +0530

    authmiddleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,29 +1,32 @@
+// auth.middleware.js
+
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const dotenv = require("dotenv");
 dotenv.config();
+
 module.exports.checkUserAuth = async (req, res, next) => {
     let token;
     const { authorization } = req.headers;
+
     if (authorization && authorization.startsWith('Bearer')) {
         try {
             // Get Token from header
-            token = authorization.split(' ')[1]
+            token = authorization.split(' ')[1];
+            console.log('Received Token:', token);
+
             // Verify Token
             const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            console.log(userID);
+            console.log('Decoded UserID:', userID);
+
             // Get User from Token
             req.user = await User.findById(userID).select('-password');
             next();
-
         } catch (err) {
-            console.log(err);
-            res.status(401).json({ "status": "failed", "message": "Unauthorized User" })
-
+            console.error('Token Verification Error:', err);
+            res.status(401).json({ "status": "failed", "message": "Unauthorized User" });
         }
+    } else {
+        res.status(401).json({ "status": "failed", "message": "Unauthorized User, No Token" });
     }
-    if (!token) {
-        res.status(401).json({ "status": "failed", "message": "Unauthorized User, No Token" })
-    }
-
-}
+};
